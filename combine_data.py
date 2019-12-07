d8eeb34151eff8488ef39513c50f1a1a71104c92
jackschu <js9pa@virginia.edu>
2019-12-07 17:52:22 -0500

    adding dataset scripts

---
+++ combine_data.py
@@ -0,0 +1,24 @@
+import csv
+
+
+def clean_path(dir_name, file_name):
+    results = []
+    rows = {}
+    with open(dir_name + file_name) as csvfile:
+        reader = csv.reader(csvfile)  # change contents to floats
+        for row in reader:  # each row is a list
+            img_name = row[0]
+            if img_name in rows:
+                rows[img_name] += row[-4:]
+            else:
+                rows[img_name] = row[1:]
+
+    with open(dir_name + "cleaned_" + file_name, "w") as file:
+        for key in rows.keys():
+            file.write(key)
+            file.write(",")
+            print(",".join(rows[key]), file=file)
+
+
+clean_path("images/test/", "test_labels.csv")
+clean_path("images/train/", "train_labels.csv")
